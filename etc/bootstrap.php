97f0328e2f00164c214a40b1fa349449bdf85fd7
rmp-up <mail+99@rmp-up.de>
2020-01-03 12:00:10 +0100

    qa: Testing for WP_CLI logging
    The WP_CLI logging part is not covered in tests yet.
    This makes it a quiet unstable part
    and hard to maintain.
    We figured out how to test this chaos
    and fully covered the use cases.

--- etc/bootstrap.php
+++ etc/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
+// There is an autoload for WP_CLI\\ but not for \WP_CL
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/wp-cli/wp-cli/php/class-wp-cli.php';
+
 \Pretzlaw\WPInt\run_wp();
